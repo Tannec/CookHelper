4a6a756c7ac51d8ebaa39b1a693dea0a0c0a26df
Arthur231456 <artur.231456@gmail.com>
2022-06-10 01:04:51 +0500

    sdsdsddfvsv

--- api/post_service/service.py
+++ api/post_service/service.py
@@ -2,21 +2,23 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from dotenv import load_dotenv
 
-load_dotenv()
+HOST = 'smtp.gmail.com'
+PORT = 465
+FROM = 'cookhelper.itl@gmail.com'
+PASSWORD = 'bcadyufotiogfduq'
 
 
 def send_mail(email, subject, text):
-    from_address = os.getenv("FROM")
-    password = os.getenv("PASSWORD")
+    from_address = FROM
+    password = PASSWORD
     msg = MIMEMultipart()
     msg["From"] = from_address
     msg["To"] = email
     msg["Subject"] = subject
     body = text
     msg.attach(MIMEText(body, "plain"))
-    server = smtplib.SMTP_SSL(os.getenv("HOST"), os.getenv("PORT"))
+    server = smtplib.SMTP_SSL(HOST, PORT)
     server.login(from_address, password)
     server.send_message(msg)
     server.quit()
